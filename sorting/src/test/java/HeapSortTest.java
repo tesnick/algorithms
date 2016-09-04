bd4196bdbb45886f15429990bf455d4cd0985a6f
tesnick <tesnick@gmail.com>
2016-09-04 18:00:05 +0200

    Added HeapSort algorithm

---
+++ sorting/src/test/java/HeapSortTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Javier on 04/09/2016.
+ */
+public class HeapSortTest {
+
+    @Test
+    public void sortTest() {
+
+        int[] unsortedArray = new int[]{5, 2, 4, 6, 1, 3};
+
+        HeapSort.sort(unsortedArray);
+
+        Assert.assertEquals(1, unsortedArray[0]);
+        Assert.assertEquals(2, unsortedArray[1]);
+        Assert.assertEquals(3, unsortedArray[2]);
+        Assert.assertEquals(4, unsortedArray[3]);
+        Assert.assertEquals(5, unsortedArray[4]);
+        Assert.assertEquals(6, unsortedArray[5]);
+
+
+        Assert.assertTrue(unsortedArray[0] < unsortedArray[1]);
+        Assert.assertTrue(unsortedArray[1] < unsortedArray[2]);
+        Assert.assertTrue(unsortedArray[2] < unsortedArray[3]);
+        Assert.assertTrue(unsortedArray[3] < unsortedArray[4]);
+        Assert.assertTrue(unsortedArray[4] < unsortedArray[5]);
+    }
+}
